030a091120ad4490d8eea36c038d773dfbc51b1d
Atharva Chavan <atharvachavan206@gmail.com>
2026-05-11 13:26:43 +0530

    added a fe dependency nothing much

--- src/main/java/com/pesto/pestoindia/controller/TaskController.java
+++ src/main/java/com/pesto/pestoindia/controller/TaskController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@RestController @ControllerAdvice
+@RestController @ControllerAdvice @CrossOrigin(origins = "*")
 public class TaskController {
     private final CustomerService customerService;
 
